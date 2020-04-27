6460ad471ea19f1b35ded67751709b9df19eeb30
Samson Shukla <52130854+s47mson@users.noreply.github.com>
2020-04-27 23:46:48 +0530

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,47 @@
+const songs = [
+  "bensound-anewbeginning.mp3",
+  "bensound-clearday.mp3",
+  "bensound-creativeminds.mp3",
+  "bensound-dance.mp3",
+  "bensound-dreams.mp3",
+  "bensound-dubstep.mp3",
+  "bensound-endlessmotion.mp3",
+  "bensound-happiness.mp3",
+  "bensound-moose.mp3",
+  "bensound-perception.mp3"
+]
+
+const createSongList = ()=> {
+  const list = document.createElement('ol')
+
+  for(let i = 0; i<songs.length; i++){
+    const item = document.createElement('li')
+    item.appendChild(document.createTextNode(songs[i]))
+
+    list.appendChild(item)
+  }
+  return list
+}
+
+document.getElementById('songList').appendChild(createSongList())
+
+songList.onclick = (e)=> {
+  const clickedItem = e.target
+  const source = document.getElementById('source')
+  source.src = './music/' + clickedItem.innerText
+
+  document.getElementById('currentlyPlayingSong').innerText = 'Currently Playing : '
+  document.getElementById('currentSong').innerText = clickedItem.innerText
+  
+  player.load()
+  player.play()
+}
+
+const playAudio = ()=> {
+  if(player.readyState){
+    player.play
+  }
+}
+const pauseAudio = ()=> {
+  player.pause
+}
